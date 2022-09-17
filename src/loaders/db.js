7ca51704e2db71504748d9baa43d16c7b11fe10b
Martarelli <renanmartarelli@gmail.com>
2022-09-17 11:01:07 -0300

    alteração na conexão

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 async function startDB(){
+    console.log("Tentando conexão ao MongoDB....")
     await mongoose.connect('mongodb+srv://rmartarelli:martarelli102030@nodejs.biydmlw.mongodb.net/test');
     console.log("Conectado MongoDB")
 }
